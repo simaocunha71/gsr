c8e3ea17a6f054a74348cf6cbd1433dd500a351c
Simão Pedro Sá Cunha <61991247+simaocunha71@users.noreply.github.com>
2023-06-20 19:09:21 +0100

    ja remove entradas

--- MIB/table.py
+++ MIB/table.py
@@ -27,16 +27,20 @@ class DataTableGeneratedKeys:
         
     def remove_expired_entries(self):
         expired_entries = []
-        
-        for key_id, (entry, timestamp) in self.dataTableGeneratedKeys.items():
-            print(f"timestamp ({timestamp}) > entry.get_field(5).get_value() ({int(entry.get_field(5).get_value())})?????????????????")
+
+        for key_id, (entry, _) in self.dataTableGeneratedKeys.items():
+            current_datetime = datetime.datetime.now()
+            timestamp = int((current_datetime - current_datetime.replace(hour=0, minute=0, second=0)).total_seconds())
+
+            #print(f"timestamp = {timestamp}")
+            #print(f"int(entry.get_field(5).get_value()) = {int(entry.get_field(5).get_value())}")
             if timestamp > int(entry.get_field(5).get_value()):
                 expired_entries.append(key_id)
-        print("-----------------------------------------")
-        
+
         for key_id in expired_entries:
             del self.dataTableGeneratedKeys[key_id]
             self.dataNumberOfValidKeys -= 1
+
     
     def to_string(self):
         """Função que imprime a tabela"""
